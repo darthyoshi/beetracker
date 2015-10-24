8abdfe55e1a7dfd41633eeed55b289378fc75ad0
Kay Choi <kay.n.choi@gmail.com>
2015-10-24 01:03:12 -0700

    The ShapeRecognizer class should now be able to detect waggle dance shapes even if the path is preceded by extraneous movement.

--- src/beetracker/ShapeRecognizer.java
+++ src/beetracker/ShapeRecognizer.java
@@ -1,14 +1,29 @@
+/**
+ * @file ShapeRecognizer.java
+ * @author Kay Choi, 909926828
+ * @date 21 Oct 15
+ * @description Provides shape recognition for waggle dance detection.
+ */
+
 package beetracker;
 
 import $N.*;
 
-import java.util.List;
+import java.util.LinkedList;
 import java.util.Vector;
 
+/**
+ *
+ * @author Kay Choi
+ */
 class ShapeRecognizer {
     NDollarRecognizer rec;
     private final boolean debug;
+    private static final float shapeScore = 0.8f;
 
+    /**
+     * Class constructor.
+     */
     ShapeRecognizer(BeeTracker parent, boolean debug) {
         this.debug = debug;
         
@@ -18,17 +33,32 @@ class ShapeRecognizer {
         rec.LoadGesture(parent.createInputRaw("paths/waggle-vert.xml"));
     }
     
-    void recognize(List<float[]> path) {
-        Vector<PointR> vec = new Vector<>();
-        
+    /**
+     * Checks a path for the waggle dance.
+     * @param path a List of float pairs representing a path
+     * @return true if the waggle dance shape is part of the path
+     */
+    boolean recognize(java.util.List<float[]> path) {
+        boolean result = false;
+
+        LinkedList<PointR> list = new LinkedList<>();
+
         for(float[] point : path) {
-            vec.add(new PointR(point[0], point[1]));
+            list.add(new PointR(point[0], point[1]));
         }
-        
-        NBestList score = rec.Recognize(vec, 1);
-        
-        if(score.getScore() >= 0.75) {
-            
+
+        //check path
+        while(!list.isEmpty()) {
+            //TODO overload NDollarRecognizer.Recognize() to accept List instead of only Vector?
+            if(rec.Recognize(new Vector<PointR>(list), 1).getScore() >= shapeScore) {
+                result = true;
+                break;
+            }
+
+            //discard path head
+            list.pop();
         }
+
+        return result;
     }
 }
